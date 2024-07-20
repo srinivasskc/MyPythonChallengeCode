343cd321e3cf8ef142b53da68715c8798942bb15
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2024-07-20 05:50:52 +0000

    my first program

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+# Print function
 print('Hello World')
+print('Hello World', 'Python is fun')
+print('Hello World', ', Python is fun')
+
+# Variables and Types
+my_integer = -2
+print(my_integer)
+
+my_float = 2.1
+print(my_float)
+
+my_string = "Hello Python"
+print(my_string)
+
+my_bool = True
+print(my_bool)
+
+# Type of Variables
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+print(type(my_bool))
+
+# Storing my name in variables
+my_firstname = 'Srinivas'
+my_lastname = "Kadiyala"
+
+# Printing the first name and last name of the user
+print(my_firstname, my_lastname)
+'''
+This is my multi line comment.
+These statements would be ignored by the compiler.
+'''
