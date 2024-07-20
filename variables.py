b6e95df9f41d13dcf7aff0c949d9d306a1740fc3
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2024-07-20 05:54:02 +0000

    Variables - Challenge 1

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Challenge 1 - Variables
+'''
+Storing name and favorite number in variables
+and printing them in a message.
+'''
+
+my_name = 'Srinivas'
+my_favourite_numer = 7
+
+# Printing the message
+print('Hello, my name is', my_name, 'and my favourite number is',
+      my_favourite_numer)
